101e15b42362e2717b5cdccde5c5c1d1f0213888
user <user@users-MacBook-Pro-1618.local>
2019-08-17 22:08:42 +0700

    FIX ICEPTI

--- get.php
+++ get.php
@@ -1,11 +1,11 @@
 <?php
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
-$filename = "coba.txt";
+$filename = "rundown.txt";
 $source_file = fopen( $filename, "r" ) or die("teu bisa muka $filename");
 while (!feof($source_file)) {
   $buffer = fread($source_file, 4096);  // use a buffer of 4KB
-  // $buffer = str_replace($old,$new,$buffer); 
+  // $buffer = str_replace($old,$new,$buffer);
   echo $buffer."\n";
   ///
 }
